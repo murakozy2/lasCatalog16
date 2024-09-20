05b5846b2c1b98c9e77d21feca31de5705a36db2
Kalicz Péter <kaliczp@gmail.com>
2024-09-20 10:53:25 +0200

    Initial test

---
+++ Catalogteszt.R
@@ -0,0 +1,9 @@
+library(lidR)
+las <- readLAS("vgyteszt/tile_638000_289000.las", select = "xyzrn")
+las <- readLAS("vgyteszt/tile_638500_289000.las", select = "xyzrn")
+las <- readLAS("vgyteszt/tile_639500_289000.las", select = "xyzrn")
+
+las <- readLAS("vgyteszt/tile_638000_290500.las", select = "xyzrn")
+las <- readLAS("vgyteszt/tile_639500_290500.las", select = "xyzrn")
+
+plot(las)
