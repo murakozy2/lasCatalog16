59df9fa5b1daf778b02f1730837cae69672974b9
Kalicz Péter <kaliczp@gmail.com>
2024-10-14 11:46:21 +0200

    Visualised error

--- Analysis.R
+++ Analysis.R
@@ -1,1 +1,2 @@
- clip_rectangle(ctg, xleft = 639115, ybottom = 289630, xright = 639251, ytop = 289787)
+ gond1 <- clip_rectangle(ctg, xleft = 639115, ybottom = 289630, xright = 639251, ytop = 289787)
+plot(gond1)
